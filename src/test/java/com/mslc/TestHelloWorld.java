071d3a23cd2593a26a28c04451c2f63f3c9ae3d0
muralibaci <murali.uppalapati@gmail.com>
2017-05-04 14:34:15 +0530

    Update TestHelloWorld.java
    to correct the build

--- src/test/java/com/mslc/TestHelloWorld.java
+++ src/test/java/com/mslc/TestHelloWorld.java
@@ -27,6 +27,6 @@ public class TestHelloWorld {
 	   {
 	      h.setName("World");
 	      assertEquals(h.getName(),"World");
-	      assertEquals(h.getMessage(),"Hello World! Murali test");
+	      assertEquals(h.getMessage(),"Hello World!");
 	   }
 }
